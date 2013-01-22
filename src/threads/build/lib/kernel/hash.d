8c3d5c2da8a01dd071128bc7b999e23b7c5dcbcc
perno902 <perno902@student.liu.se>
2013-01-22 14:38:22 +0100

    "add files"

---
+++ src/threads/build/lib/kernel/hash.d
@@ -0,0 +1,3 @@
+lib/kernel/hash.o: ../../lib/kernel/hash.c ../../lib/kernel/hash.h \
+  ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+  ../../lib/kernel/list.h ../../lib/debug.h ../../threads/malloc.h
